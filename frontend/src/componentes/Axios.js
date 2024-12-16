14e939c5ed3556483fee6db8b3477df8e089a2fa
David <julian.david.rengifo@correounivalle.edu.co>
2024-12-15 22:02:31 -0500

    se creo un archivo env para manejar el host de render y se modifico axios para que use el host local y de despliegue

--- frontend/src/componentes/Axios.js
+++ frontend/src/componentes/Axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8000/";
+const baseUrl = process.env.REACT_APP_BASE_URL || "http://127.0.0.1:8000/";
+
 const AxiosInstance = axios.create({
   baseURL: baseUrl,
   timeout: 5000,
