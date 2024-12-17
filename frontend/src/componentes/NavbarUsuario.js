c25a5286eb27dfe3a25d6fb21f500af1c0ef7ada
JuanFDJ73 <vavodri@gmail.com>
2024-12-17 14:07:22 -0500

    Agregar boton "Cerrar sesion".

--- frontend/src/componentes/NavbarUsuario.js
+++ frontend/src/componentes/NavbarUsuario.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavbarUsuario = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token"); // Elimina el token del localStorage
+    navigate("/login"); // Redirige al usuario a la página de inicio de sesión
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: "#6a1b9a" }}>
         <Toolbar>
           <Typography
             variant="h6"
-            sx={{ flexGrow: 1, color: "#ffffff", textDecoration: "none" }}
-            component={Link}
-            to="/"
+            sx={{
+              flexGrow: 1,
+              color: "#ffffff",
+              textDecoration: "none",
+              cursor: "pointer",
+            }}
+            onClick={handleLogout} // Ahora el logo cierra sesión
           >
             Venta Caninos
           </Typography>
+
           <Button
             component={Link}
             to="/inicio"
@@ -42,6 +54,13 @@ const NavbarUsuario = () => {
             sx={{ color: "#ffffff", textDecoration: "none" }}
           >
             Estadísticas
+          </Button>
+          {/* Botón de Cerrar Sesión */}
+          <Button
+            onClick={handleLogout}
+            sx={{ color: "#ffffff", textDecoration: "none" }}
+          >
+            Cerrar Sesión
           </Button>
         </Toolbar>
       </AppBar>
